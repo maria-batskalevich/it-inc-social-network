4deb4c071e666caa75948b47838e994af7449ca2
maria-batskalevich <maria.batskalevich@gmail.com>
2022-01-24 19:30:20 +0300

    samurai path 1.0 85 add useEffect to ProfileStatusWithHooks

--- src/components/Dialogues/Dialogues.tsx
+++ src/components/Dialogues/Dialogues.tsx
@@ -4,42 +4,41 @@ import {
     DialogueItem,
 } from "./DialogueItem/DialogueItem";
 import {Message} from "./Message/Message";
-import AddMessageForm, {AddMessageFormDataType} from "./AddNessageForm/AddMessageForm";
+import AddMessageForm, {AddMessageFormDataType} from "./AddMessageForm/AddMessageForm";
 import {DialoguesPropsType} from "./DialoguesContainer";
 
 export const Dialogues = React.memo((props: DialoguesPropsType) => {
 
-    const dialogueElements = props.dialogues.map((dialogue) => (
-        <DialogueItem
-            key={dialogue.id}
-            personName={dialogue.personName}
-            id={dialogue.id}/>
-    ));
+        const dialogueElements = props.dialogues.map((dialogue) => (
+            <DialogueItem
+                key={dialogue.id}
+                personName={dialogue.personName}
+                id={dialogue.id}/>
+        ));
 
-    const messageElements = props.messages.map((message) => (
-        <Message
-            key={message.id}
-            messageText={message.messageText}
-            id={message.id}/>
-    ));
+        const messageElements = props.messages.map((message) => (
+            <Message
+                key={message.id}
+                messageText={message.messageText}
+                id={message.id}/>
+        ));
 
-    const addNewMessage = (values: AddMessageFormDataType) => {
-        props.sendMessage(values.newMessageText);
-    };
+        const addNewMessage = (values: AddMessageFormDataType) => {
+            props.sendMessage(values.newMessageText);
+        };
 
-
-    return (
-        <div className={s.dialoguesPage}>
-            <div className={s.dialoguesList}>{dialogueElements}</div>
-            <div className={s.messagesList}>
-                <div>{messageElements}</div>
-                <div>
-                    <AddMessageForm onSubmit={addNewMessage}/>
+        return (
+            <div className={s.dialoguesPage}>
+                <div className={s.dialoguesList}>{dialogueElements}</div>
+                <div className={s.messagesList}>
+                    <div>{messageElements}</div>
+                    <div>
+                        <AddMessageForm onSubmit={addNewMessage}/>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
 )
 
 
